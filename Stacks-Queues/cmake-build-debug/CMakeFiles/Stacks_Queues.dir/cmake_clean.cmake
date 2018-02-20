5e171f806cdcc38778b37eea8177a808bd77c018
theRealScoobaSteve <stephensimone@live.com>
2018-02-19 20:09:19 -0800

    Fixed folder structure

---
+++ Stacks-Queues/cmake-build-debug/CMakeFiles/Stacks_Queues.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stacks_Queues.dir/main.cpp.obj"
+  "Stacks_Queues.pdb"
+  "Stacks_Queues.exe"
+  "Stacks_Queues.exe.manifest"
+  "libStacks_Queues.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stacks_Queues.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Stacks-Queues/cmake-build-debug/CMakeFiles/Stacks_Queues.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Stacks_Queues.
+# This may be replaced when dependencies are built.
